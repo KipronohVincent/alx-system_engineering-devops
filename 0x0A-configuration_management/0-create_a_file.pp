ad49f9b1eee7b796b71795435a05db9c12657bf0
KipronohVincent <kipronohvincent43@gmail.com>
2022-09-16 11:16:34 +0300

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# creates a file in the /tmp directory
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}

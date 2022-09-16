0d1d66b6e37d38d7165e8e830ec5129df74e0c13
KipronohVincent <kipronohvincent43@gmail.com>
2022-09-16 12:19:51 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-#Install flask / Version must be 2.1.0
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# installs the package puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }

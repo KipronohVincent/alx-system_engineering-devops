2caca4157ea3cfd1d55a706b91c355ce02eed266
KipronohVincent <kipronohvincent43@gmail.com>
2022-11-01 10:39:21 +0300

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A function that queries the Reddit API and
+returns the number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
